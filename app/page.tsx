ffe488850b418323c835da74e176d274f55e738f
Alex <thadarknight@192.168.2.37>
2024-11-07 12:55:26 +0700

    backlog

--- app/page.tsx
+++ app/page.tsx
@@ -4,10 +4,8 @@ import PomodoroPlanner from './components/PomodoroPlanner'
 export default function Home() {
   return (
     <main className="min-h-screen flex flex-col">
-      <div className="h-screen">
-        <HeroSection />
-      </div>
-      <section className="bg-indigo-800 py-8">
+      <HeroSection />
+      <section className="flex-grow bg-indigo-800 py-8">
         <div className="container mx-auto px-4">
           <PomodoroPlanner />
         </div>
